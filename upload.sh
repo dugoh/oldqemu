297b96f88435fc3c5fbec5b83fcc4948dd899d54
Jacob Goense <dugo@xs4all.nl>
2016-10-11 15:00:32 +0200

    Fix Travis upload to gh-pages

--- upload.sh
+++ upload.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-tar -cvf qemu.tar ./qemu/
-bzip2 --best qemu.tar
-mkdir page
-cd page
+tar -cf qemu.tar ./qemu/ && bzip2 --best qemu.tar
+mkdir page && cd page
 mv ../qemu.tar.bz2 ./
-echo git init
+
 git init
-echo git config user.name "${USER}"
 git config user.name "${USER}"
-echo git config user.email "${GHP_MAIL}"
-git config user.email "${GHP_MAIL}"
+git config user.email "${USER}@travis-ci.org"
 
 echo "<HTML><HEAD><TITLE>LINKS</TITLE></HEAD><BODY><ul>" >index.html
 for file in $(ls|grep -v index.html); do \
@@ -23,10 +19,7 @@ for file in $(ls|grep -v index.html); do \
 done
 echo "</ul></BODY></HTML>" >>index.html
 
-echo git add .
 git add .
-echo git commit -m "Deploy to GitHub Pages"
 git commit -m "Deploy to GitHub Pages"
-echo git push --force --quiet https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages
 git push --force --quiet https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages
 cd ..
