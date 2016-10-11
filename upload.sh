d7ae76e47032fe23b81a2339c06513e22f7a2582
Jacob Goense <dugo@xs4all.nl>
2016-10-11 02:59:47 -0400

    uploader to gh-pages [skip ci]

---
+++ upload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+tar -cvf qemu.tar ./qemu/
+bzip2 --best qemu.tar
+mkdir page
+cd page
+mv ../qemu.tar.bz2 ./
+git init
+git config user.name "${USER}"
+git config user.email "${GHP_MAIL}"
+echo "<HTML><HEAD><TITLE>LINKS</TITLE></HEAD><BODY><ul>" >index.html
+for file in $(ls|grep -v index.html); do \
+  (\
+    printf '<li><a href="' ; \
+    printf "${file}" ; \
+    printf '">' ; \
+    printf "${file}" ; \
+    printf '</a></li>\n' \
+  ) >>index.html ; \
+done
+echo "</ul></BODY></HTML>" >>index.html
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages
+cd ..
