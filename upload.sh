f16a99bd18d19ad5b305b226494356188fa404a3
Jacob Goense <dugo@xs4all.nl>
2016-10-11 14:46:02 +0200

    Debug Travis

--- upload.sh
+++ upload.sh
@@ -4,9 +4,13 @@ bzip2 --best qemu.tar
 mkdir page
 cd page
 mv ../qemu.tar.bz2 ./
+echo git init
 git init
+echo git config user.name "${USER}"
 git config user.name "${USER}"
+echo git config user.email "${GHP_MAIL}"
 git config user.email "${GHP_MAIL}"
+
 echo "<HTML><HEAD><TITLE>LINKS</TITLE></HEAD><BODY><ul>" >index.html
 for file in $(ls|grep -v index.html); do \
   (\
@@ -18,7 +22,11 @@ for file in $(ls|grep -v index.html); do \
   ) >>index.html ; \
 done
 echo "</ul></BODY></HTML>" >>index.html
+
+echo git add .
 git add .
+echo git commit -m "Deploy to GitHub Pages"
 git commit -m "Deploy to GitHub Pages"
+echo git push --force --quiet https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages
 git push --force --quiet https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages
 cd ..
