965e04c3746de6d444e3d0fb972ef83a96f531a1
Jacob Goense <dugo@xs4all.nl>
2016-10-14 05:25:08 -0400

    debug pic version

--- build.sh
+++ build.sh
@@ -44,6 +44,7 @@ cd qemu
 check going back to 0.11;        git reset --hard 08fd2f30bd3ee5d04596da8293689af4d4f7eb6c    >/dev/null 2>&1 && ok || nok
 check remove definition of BIT;  sed -i -e 's/#define BIT.n. .1 << .n../\/\/&/' hw/eepro100.c >/dev/null 2>&1 && ok || nok
 check define BIT properly;       printf "#ifndef BIT\n#define BIT(n) (1 << (n))\n#endif\n" >> qemu-common.h   && ok || nok
+check turn on pic debugging;     sed -i -e 's/\/\/#define DEBUG_PIC/#define DEBUG_PIC/' hw/i8259.c            && ok || nok
 check configure qemu;            ./configure --target-list=i386-softmmu \
                                              --disable-sdl \
                                              --disable-vnc-tls \
