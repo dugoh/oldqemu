869a46027b2b455d5a1f99809c369156b208200c
Jacob Goense <dugo@xs4all.nl>
2019-03-15 10:50:27 +0100

    Cleanup and correct exit code

--- upload.sh
+++ upload.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-cd /root/
-tar -cf qemu.tar ./qemu/ && bzip2 --best qemu.tar
-mkdir page && cd page
-mv ../qemu.tar.bz2 ./
+# Poor Man's artifact dump. Tar up the build and upload to github pages.
+
+GITHUB_URL="https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+
+cd /root/                || exit 1
+tar -cf qemu.tar ./qemu/ || exit 1
+bzip2 --best qemu.tar    || exit 1
+mkdir page               || exit 1
+cd page                  || exit 1
+mv ../qemu.tar.bz2 ./    || exit 1
+
+echo "<HTML><HEAD><TITLE>LINKS</TITLE></HEAD><BODY><ul>" >index.html
+for file in $(ls | egrep -v "^index.html$| "); do \
+    (\
+        printf '<li><a href="'; \
+        printf "${file}";       \
+        printf '">';            \
+        printf "${file}";       \
+        printf '</a></li>\n'    \
+    )>>index.html;              \
+done
+echo "</ul></BODY></HTML>" >>index.html
 
 git init
 git config user.name "${USER}"
 git config user.email "${USER}@travis-ci.org"
-
-echo "<HTML><HEAD><TITLE>LINKS</TITLE></HEAD><BODY><ul>" >index.html
-for file in $(ls|grep -v index.html); do \
-  (\
-    printf '<li><a href="' ; \
-    printf "${file}" ; \
-    printf '">' ; \
-    printf "${file}" ; \
-    printf '</a></li>\n' \
-  ) >>index.html ; \
-done
-echo "</ul></BODY></HTML>" >>index.html
-
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages
-cd ..
+git push --force --quiet "${GITHUB_URL}" master:gh-pages
+exit $?
